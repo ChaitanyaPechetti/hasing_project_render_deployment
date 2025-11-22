5e2814f10fa32350e276afc59f06d3f2b47039a2
ChaitanyaPechetti <pechettichaitanya123@gmail.com>
2025-11-22 12:05:54 +0530

    allowedhost

--- hasing_project/settings.py
+++ hasing_project/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hasing-project-render-deployment.onrender.com','localhost:8000'] # * -> allowed any host
 
 
 # Application definition
